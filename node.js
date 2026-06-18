ce2d7fd4e5cde8640495a46e9579bba23606e730
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-06-18 18:34:17 +0530

    updated index

--- node.js
+++ node.js
@@ -6,3 +6,35 @@ let student = {
   course: "JavaScript",
   city: "Palakkad"
 };
+// Display Profile on Webpage
+function showProfile() {
+
+document.getElementById("name").innerText = 
+student.name;
+
+document.getElementById("age").innerText = 
+"Age : " + student.age;
+
+document.getElementById("course").innerText =
+"Course : " + student.course;
+
+document.getElementById("city").innerText =
+"City : "+ student.city;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//     document.getElementById("city").innerText =
+//     "City : " + student.city;
+
+// }
